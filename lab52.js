d92f846231f84b736f5dc2a39aaa736ccdde15b2
twisstr <twisstr@g.crookcountyschools.org>
2021-03-15 19:56:43 -0700

    lab 52 completed

---
+++ lab52.js
@@ -0,0 +1,13 @@
+let numbers = [50,10,33,6,67,94,27,3,9,5]
+for (let i=0; i < numbers.length; i++) {
+    console.log(numbers[0]*numbers[0])
+    console.log(numbers[1]*numbers[1])
+    console.log(numbers[2]*numbers[2])
+    console.log(numbers[3]*numbers[3])
+    console.log(numbers[4]*numbers[4])
+    console.log(numbers[5]*numbers[5])
+    console.log(numbers[6]*numbers[6])
+    console.log(numbers[7]*numbers[7])
+    console.log(numbers[8]*numbers[8])
+    console.log(numbers[9]*numbers[9])
+}
